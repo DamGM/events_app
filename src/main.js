1c109722c0f6094228d433ba6192750212e2b745
Dámaris Guzmán <damarisguzman@MacBook-Air-de-Damaris.local>
2024-09-22 14:23:17 +0200

    autentificacion cambiada

--- src/main.js
+++ src/main.js
@@ -3,16 +3,14 @@ import App from './App.vue';
 import router from './router';
 import 'bulma/css/bulma.css';
 
-// Import the Auth0 configuration
-import authConfig from '../auth_config.json'; // Import the whole JSON file as an object
 
 // Import the plugin here
 import { Auth0Plugin } from './auth';
 
 // Install the authentication plugin here
 Vue.use(Auth0Plugin, {
-  domain: authConfig.domain,  // Access properties from the imported object
-  clientId: authConfig.clientId, // Access properties from the imported object
+  domain: process.env.VUE_APP_AUTH0_DOMAIN,  // Access from environment variables
+  clientId: process.env.VUE_APP_AUTH0_CLIENT_ID, // Access from environment variables
   onRedirectCallback: (appState) => {
     router.push(
       appState && appState.targetUrl
